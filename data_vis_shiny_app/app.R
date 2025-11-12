bd8e25d8e9d27196d82ddde2a12e7e40f89be417
ASHgithubb <anneshmail@gmail.com>
2025-11-12 15:17:09 +0100

    edited map!

--- data_vis_shiny_app/app.R
+++ data_vis_shiny_app/app.R
@@ -2,9 +2,11 @@ library(shiny)
 library(bslib)
 library(ggplot2)
 library(leaflet)
+library(leafgl)
 
 # ---- Load Data and Functions ----
 df_clean <- read.csv("df_clean.csv", stringsAsFactors = FALSE)
+
 
 # ---- Define UI ----
 ui <- fluidPage(
@@ -88,37 +90,19 @@ ui <- fluidPage(
   card(
     card_body(
       div(
-        plotOutput("temp", height = "100%", width = "100%"),
+        # Create a CSS grid: 6 columns, 3 rows
         style = "
-          display: flex;
-          justify-content: space-between;
-          gap: 10px;
-          background-color: #cceeff;
-          border: 1px solid #00000040;
-          width: 1500px;
-          height: 500px;
-          justify-content: center;
-          align-items: center;
-          font-size: 20px;"
-      )
-    )
-  ),
-  
-  card(
-    card_body(
-      div(
-        leafletOutput("map"),
-        style = "
-          display: flex;
-          justify-content: space-between;
-          gap: 10px;
-          background-color: #cceeff;
-          border: 1px solid #00000040;
-          width: 1500px;
-          height: 500px;
-          justify-content: center;
-          align-items: center;
-          font-size: 20px;"
+        display: grid;
+        grid-template-columns: repeat(6, 2fr);
+        grid-template-rows: repeat(3, 100px);
+        gap: 10px;
+        width: 100%;
+      ",
+        
+        # Generate 18 leafletOutput placeholders dynamically
+        lapply(1:18, function(i) {
+          leafletOutput(paste0("map", i), width = "100%", height = "100%")
+        })
       )
     )
   )
@@ -162,56 +146,75 @@ server <- function(input, output) {
     histogram_continous(x = "year", title = "Year", df = df_clean)
   })
   
-  # Main plot that switches by selected variable
-  output$temp <- renderPlot({
-    x <- switch(
-      input$var,
-      "Happiness" = "happiness",
-      "Education" = "educ",
-      "Race" = "race",
-      "Marital Status" = "marital",
-      "Work" = "work"
-    )
-    
-    title <- switch(
-      input$var,
-      "Happiness" = "Happiness Levels",
-      "Education" = "Educational Levels",
-      "Race" = "Race Distribution",
-      "Marital Status" = "Marital Status",
-      "Work" = "Work Distribution"
-    )
-    
-    temp_plot_year(x = x, df = df_clean, title = title)
-  })
   
-  # Map plot 
-  output$map <- renderLeaflet({
-    leaflet(map_data) %>%
-      addTiles() %>%
-      setView(lng = -98.5, lat = 39.8, zoom = 4) %>%
-      addPolygons(
-        fillColor = ~region_palette(region),
+  # Map plot
+  
+  observe({
+    variable <- switch(input$var,
+                       "Happiness" = "happiness",
+                       "Education" = "educ",
+                       "Race" = "race",
+                       "Marital Status" = "marital",
+                       "Work" = "work")
+    my_maps_list <- switch(variable,
+                       "happiness" = as.list(sprintf("map_files_happy/map_happy_%d.shp", 1:18)),
+                       "educ" = rep(list("map_files/map_happy.shp"), 18),
+                       "race" = rep(list("map_files/map_happy.shp"), 18),
+                       "marital" = rep(list("map_files/map_happy.shp"), 18),
+                       "work" = rep(list("map_files/map_happy.shp"), 18))
+    
+    legend_titles <- switch(variable,
+                           "happiness" = rep(list("pretty happy"), 18),
+                           "educ" = rep(list("pretty happy"), 18),
+                           "race" = rep(list("pretty happy"), 18),
+                           "marital" = rep(list("pretty happy"), 18),
+                           "work" = rep(list("pretty happy"), 18))
+    
+  
+  for (i in seq_along(my_maps_list)) {
+    local({
+      my_i <- i
+      map_file <- my_maps_list[[my_i]]          # dataframe for this map
+      legend_title_i <- legend_titles[[my_i]]
+      output[[paste0("map", my_i)]] <- renderLeaflet({
+
+
+    #Read the correct shapefile
+    my_map <- sf::read_sf(map_file)
+    
+
+    leaflet(my_map, options = leafletOptions(zoomControl = FALSE, dragging = FALSE)) %>%
+      setView(lng = -98.5, lat = 39.8, zoom = 2) %>%
+      addGlPolygons(
+        data = my_map,
+        fillColor = ~region_palette(percent),
         color = "black",
         weight = 1,
         opacity = 1,
         fillOpacity = 0.7,
+        label = ~paste0(region, ": ", percent, "%"),
         highlightOptions = highlightOptions(
           weight = 2,
           color = "#666",
           fillOpacity = 0.9,
           bringToFront = TRUE
         )
-      ) %>%
-      addLegend(
-        pal = region_palette,
-        values = map_data$region,
-        title = "Region",
-        opacity = 1
       )
-  })
+      })
+      
+    })
+  }
+  }) # end observe
 }
 
 # ---- Run the App ----
 shinyApp(ui = ui, server = server)
+
+
+# %>%
+#   addLegend(
+#     pal = region_palette,
+#     values = my_map$percent,
+#     title = legend_title,
+#     opacity = 1
 
